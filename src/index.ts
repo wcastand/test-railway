35a6688e9d6271a7fa537b0aaf41b84a0cacd94b
wcastand <wcastand@gmail.com>
2023-02-12 16:34:11 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-import { serve } from "https://deno.land/std@0.176.0/http/server.ts"
+import { serve } from 'https://deno.land/std@0.176.0/http/server.ts';
+import { Redis } from 'https://deno.land/x/upstash_redis@v1.19.3/mod.ts';
+import { Query } from 'https://deno.land/x/sql_builder/mod.ts';
+import { connect } from 'npm:@planetscale/database@^1.4';
 
-const port = parseInt(Deno.env.get("PORT") ?? "8000");
+const redis = new Redis({
+	url: Deno.env.get('UPSTASH_REDIS_REST_URL') ?? '',
+	token: Deno.env.get('UPSTASH_REDIS_REST_TOKEN') ?? '',
+});
 
-serve((_req) => new Response("Hello, world"), { port });
+const config = {
+	host: Deno.env.get('HOST'),
+	username: Deno.env.get('USERNAME'),
+	password: Deno.env.get('PASSWORD'),
+};
+
+const conn = connect(config);
+
+const port = parseInt(Deno.env.get('PORT') ?? '8000');
+
+serve(async (_req) => {
+	await redis.set('foo', crypto.randomUUID());
+	const builder = new Query();
+	const product = builder.table('products').select('*').build();
+
+	const redisRes = await redis.get<string>('foo');
+	const results = await conn.execute(product);
+
+	return new Response(JSON.stringify({ redis: redisRes, planet: results.rows }), {
+		headers: { 'content-type': 'application/json' },
+	});
+}, { port });
