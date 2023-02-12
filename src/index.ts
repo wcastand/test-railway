8de8d3f2bcce0843f835cf5a01758a9d993d0ecf
wcastand <wcastand@gmail.com>
2023-02-12 16:02:48 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
-import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.176.0/http/server.ts"
 
-const handler = async (request: Request): Promise<Response> => {
-  const resp = await fetch("https://api.github.com/users/denoland", {
-    // The init object here has an headers object containing a
-    // header that indicates what type of response we accept.
-    // We're not specifying the method field since by default
-    // fetch makes a GET request.
-    headers: {
-      accept: "application/json",
-    },
-  });
+const port = parseInt(Deno.env.get("PORT") ?? "8000");
 
-  return new Response(resp.body, {
-    status: resp.status,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-};
-
-serve(handler);
+serve((_req) => new Response("Hello, world"), { port });
